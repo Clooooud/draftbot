abbf187b3ac20a00b5e0beccbe0f60e365d89229
Yohan BARET <yohan.baret@etu.univ-grenoble-alpes.fr>
2025-05-09 00:38:42 +0200

    Settings comment

--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
+# The discord server id
 GUILD_ID = 1247849595967508511
+# The roles that can use the draft commands
 ADMIN_ROLES = [1247863670948495391, 1247863541998948413]
 
+# For each milestone, the bot will send a message in the channel
 TIMER_MILESTONES = [60, 30, 10, 5]
 
 # This will automatically recover the bot if it crashes using the last state saved inside the state.txt file
@@ -9,5 +12,7 @@ AUTO_RECOVER = True
 # "en" or "fr"
 LANGUAGE = "fr"
 
+# Tournament acronym
 TOURNAMENT_NAME = "RAWR"
+# Tournament icon link
 TOURNAMENT_ICON = "https://cdn.discordapp.com/icons/1247849595967508511/a45ca9553a36c44140c610b5721b462f.webp"
